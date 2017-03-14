d9e4cba0cd69bec02ff57a67241c82511398a481
Joy Holowicki <joyholowicki@gmail.com>
2017-03-14 15:42:56 -0500

    More ES6

--- app/app.js
+++ app/app.js
@@ -5,11 +5,13 @@ import { Main } from './components/Main'
 import { Whoops404 } from './components/Whoops404'
 import { Router, Route, hashHistory } from 'react-router'
 
+window.React = React
 // Renders the contents according to the route page.
 render(
     <Router history={hashHistory}>
         <Route path="/" component={Main}/>
-        <Route path="saved" component={Main}/>
+        <Route path=":saved" component={Main}/>
+        </Route>
         <Route path="search" component={Main}/>
         <Route path="*" component={Whoops404}/>
     </Router>
--- app/components/Main.js
+++ app/components/Main.js
@@ -1,25 +1,38 @@
 //  * **Main** - contains the main-container div that holds the main layout and navigation. This component should also be able to hold sub-components Search and Saved
 
 // Include React
-import { Component } from 'react'
+import { React } from 'react'
 import { search } from './children/search'
 import { saved } from './children/saved'
 
-export class Main extends Component {
-
+export const Main= React.createClass( {
+  dateFormatted(datestr) {
+    arr= datestr.split();
+    year = arr[0]+ arr[1] + arr[2]+ arr[3];
+    month = arr[4]+ arr[5];
+    day = arr[6]+arr[7];
+    return year + "-" + month + "-" + day
+  },
   // Here we render the function
   render() {
-    return (  
+    return (
       <div className="app">
         {(this.props.location.pathname === "/") ?
         : 
-      <div>
-        {/*layout goes here*/}
-        </div>    
+          <div>
+            <div>
+              <header>
+                {/*header goes here*/}
+              </header>
+            </div>
+            <div>
+              {/*search here*/}
+            </div>
+            <div>
+              {/*saved here*/}
+              {/*needs dateFormatted from db*/}
+            </div>
+          </div>    
       </div>
 
-    )
-  }
-
-// Export the componen back for use in other files
-module.exports = Main;
+        }))
